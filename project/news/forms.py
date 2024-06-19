248dc2f0481ae665dcd0a26bd9c9f54f00fb77de
Igor <igoroshust75@gmail.com>
2024-06-19 13:03:32 +0900

    feat: i18n modelform

--- project/news/forms.py
+++ project/news/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.core.exceptions import ValidationError
 from .models import Article, Category
+from django.utils.translation import gettext_lazy as _
 
 class NewsForm(forms.ModelForm):
     class Meta:
@@ -10,6 +11,12 @@ class NewsForm(forms.ModelForm):
             'text',
             'category',
         ]
+        labels = {
+            'name': _('Name'),
+            'text': _('Text'),
+            'category': _('Category'),
+        }
+
 
         def clean(self):
             cleaned_data = super().clean()
